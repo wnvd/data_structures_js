7a683e01ad2c5fc3ed06cac8c34274b52bf6e4aa
Naveed Wani <waninaveed998@gmail.com>
2023-03-30 16:38:33 +0530

    bubble sort

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,3 +1,14 @@
-function bSort(arr) {}
+function bSort(arr) {
+	for (let i = 0; i < arr.length; ++i) {
+		for (let j = i + 1; j < arr.length - 1; j++) {
+			if (arr[i] > arr[j]) {
+				const temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	return arr;
+}
 
-console.log(bSort([]));
+console.log(bSort([1, 4, 3, 2, 5, 6, 3, 6]));
