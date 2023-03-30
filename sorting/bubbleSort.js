71e20ffe5d2590b9053adfe357361bbc83d65dc2
Naveed Wani <waninaveed998@gmail.com>
2023-03-30 17:51:34 +0530

    smart version and optimised version of bubble sort

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,14 +1,53 @@
+// naive approach
 function bSort(arr) {
 	for (let i = 0; i < arr.length; ++i) {
-		for (let j = i + 1; j < arr.length - 1; j++) {
-			if (arr[i] > arr[j]) {
-				const temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
+		for (let j = 0; j < arr.length; j++) {
+			console.log(arr, arr[j], arr[j + 1]);
+			if (arr[j] > arr[j + 1]) {
+				const temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	return arr;
+}
+// console.log(bSort([1, 4, 3, 2, 5, 6, 3]));
+
+// better approach
+function bSort2(arr) {
+	for (let i = arr.length; i > 0; i--) {
+		for (let j = 0; j < i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				const temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
 			}
 		}
 	}
 	return arr;
 }
 
-console.log(bSort([1, 4, 3, 2, 5, 6, 3, 6]));
+console.log(bSort2([1, 4, 3, 3]));
+
+// optimzed bubble sort
+// here we are adding a flag (noSwaps)
+// if approach works if array in partially sorted
+function bSort3(arr) {
+	let noSwaps;
+	for (let i = arr.length; i > 0; i--) {
+		noSwaps = true;
+		for (let j = 0; j < i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				const temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				noSwaps = false;
+			}
+		}
+		if (noSwaps) break;
+	}
+	return arr;
+}
+
+console.log(bSort3([1, 4, 3, 3]));
