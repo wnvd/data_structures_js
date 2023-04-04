4c0a62bef4585e030b0c7592a51825a395848ea3
Naveed Wani <waninaveed998@gmail.com>
2023-04-04 17:56:41 +0530

    create node and doubly linked list also add push method"

---
+++ singlyLinkedList/dLinkedLIst.js
@@ -0,0 +1,35 @@
+class Node {
+	constructor(data) {
+		this.data = data;
+		this.next = null;
+		this.prev = null;
+	}
+}
+
+class dLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+	push(data) {
+		let node = new Node(data);
+		if (!this.head) {
+			this.head = node;
+			this.tail = node;
+		} else {
+			this.tail.next = node;
+			node.prev = this.tail;
+			this.tail = node;
+		}
+		this.length++;
+
+		return this;
+	}
+}
+
+const list = new dLinkedList();
+list.push(0);
+list.push(1);
+list.push(2);
+list.push(3);
