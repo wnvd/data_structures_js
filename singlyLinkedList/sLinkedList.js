58a380cf971f09f8b9516ad1e4af80c080a4a83f
Naveed Wani <waninaveed998@gmail.com>
2023-04-04 12:44:39 +0530

    push method on sinlgy linked list

--- singlyLinkedList/sLinkedList.js
+++ singlyLinkedList/sLinkedList.js
@@ -11,4 +11,22 @@ class singlyLinkedList {
 		this.tail = null;
 		this.length = 0;
 	}
+
+	// add node to linked list
+	push(data) {
+		let curr = new Node(data);
+		if (!this.head) {
+			this.head = curr;
+			this.tail = curr;
+		} else {
+			this.tail.next = curr;
+			curr = this.tail;
+		}
+		this.length++;
+		return this;
+	}
 }
+
+const list = new singlyLinkedList();
+list.push(1);
+list.push(2);
