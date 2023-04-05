a1bfa83523d7831b344a846fdec8c06a2b2664cd
Naveed Wani <waninaveed998@gmail.com>
2023-04-05 18:56:41 +0530

    add deQueue() to the node queue

--- stacksAndQueue/queueNode.mjs
+++ stacksAndQueue/queueNode.mjs
@@ -15,8 +15,8 @@ class Queue {
 	enqueue(data) {
 		let eNode = new Node(data);
 		if (this.size === 0) {
-			this.front = data;
-			this.back = data;
+			this.front = eNode;
+			this.back = eNode;
 		} else {
 			this.back.next = eNode;
 			this.back = eNode;
@@ -25,4 +25,33 @@ class Queue {
 
 		return eNode;
 	}
+	deQueue() {
+		if (this.size === 0) return null;
+
+		let dNode = this.front;
+		this.front = this.front.next;
+
+		this.size--;
+		return dNode;
+	}
+
+	// for debug
+	display() {
+		let curr = this.front;
+		while (curr) {
+			console.log(curr.data);
+			curr = curr.next;
+		}
+		console.log('---');
+	}
 }
+
+let newQueue = new Queue();
+newQueue.enqueue(1);
+newQueue.enqueue(2);
+newQueue.enqueue(3);
+newQueue.display();
+newQueue.deQueue();
+newQueue.display();
+newQueue.enqueue(4);
+newQueue.display();
