df657ef9e34ea723c46309c5db452ba935695f8d
Naveed Wani <waninaveed998@gmail.com>
2023-04-06 16:27:15 +0530

    insert() method for bst

--- binarySearchTree/bstInto.mjs
+++ binarySearchTree/bstInto.mjs
@@ -5,8 +5,38 @@ class Node {
 		this.right = null;
 	}
 }
-class binarySearchTree {
+class BinarySearchTree {
 	constructor() {
 		this.root = null;
+	}
+
+	insert(data) {
+		let newNode = new Node(data);
+		if (this.root === null) {
+			this.root = newNode;
+			return this;
+		} else {
+			let curr = this.root;
+			while (true) {
+				// for duplicated
+				if (value === curr.value) return undefined;
+
+				if (value < curr.value) {
+					if (curr.lef === null) {
+						curr.left = newNode;
+						return this;
+					} else {
+						curr = curr.left;
+					}
+				} else if (value > curr.value) {
+					if (curr.right === null) {
+						curr.right = newNode;
+						return this;
+					} else {
+						curr = curr.right;
+					}
+				}
+			}
+		}
 	}
 }
